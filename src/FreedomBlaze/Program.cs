0d491d68e8cf6df5f3f30baac3883a47abb47109
Marcos Almeida <malmeida@softjourn.com>
2024-05-24 19:45:12 -0300

    add azure key vault configuration to app

--- src/FreedomBlaze/Program.cs
+++ src/FreedomBlaze/Program.cs
@@ -1,3 +1,4 @@
+using Azure.Identity;
 using FreedomBlaze.Components;
 using FreedomBlaze.Infrastructure;
 using FreedomBlaze.Interfaces;
@@ -55,6 +56,12 @@ else
     //app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+if (builder.Environment.IsProduction())
+{
+    builder.Configuration.AddAzureKeyVault(
+        new Uri($"https://{builder.Configuration["KeyVaultName"]}.vault.azure.net/"),
+        new DefaultAzureCredential());
 }
 
 app.UseHttpsRedirection();
