04748d792d5a9dc1f869c9fa33306c22967206c4
Marcos Almeida <malmeida@softjourn.com>
2024-05-27 16:26:18 -0300

    change KeyVaultName

--- src/FreedomBlaze/Program.cs
+++ src/FreedomBlaze/Program.cs
@@ -59,6 +59,7 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 if (builder.Environment.IsProduction())
 {
     builder.Configuration.AddAzureKeyVault(
