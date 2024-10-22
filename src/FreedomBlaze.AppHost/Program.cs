607a150ff2a2fecffebf120d4711a52e6bd728e8
Marcos Almeida <malmeida@softjourn.com>
2024-10-22 18:28:06 -0300

    layout changes disable Phoenixd background service

--- src/FreedomBlaze.AppHost/Program.cs
+++ src/FreedomBlaze.AppHost/Program.cs
@@ -1,9 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddDockerfile("freedomblaze-phoenixd", "../submodules/phoenixd.NET/Phoenixd.NET/.docker/phoenixd")
-                     .WithBindMount("freedomblaze-phoenixd-config", "/phoenix/.phoenix")
-                     .WithEndpoint(port: 9741, targetPort: 9740)
-                     .PublishAsContainer();
+//builder.AddDockerfile("freedomblaze-phoenixd", "../submodules/phoenixd.NET/Phoenixd.NET/.docker/phoenixd")
+//                     .WithBindMount("freedomblaze-phoenixd-config", "/phoenix/.phoenix")
+//                     .WithEndpoint(port: 9741, targetPort: 9740)
+//                     .PublishAsContainer();
 
 builder.AddProject<Projects.FreedomBlaze>("freedomblaze").WithExternalHttpEndpoints();
 
