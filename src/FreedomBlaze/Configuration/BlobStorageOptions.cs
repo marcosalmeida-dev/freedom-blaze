d829447cab2b56f022f88591502941f403ce8159
Marcos Almeida <marcosalmeida.developer@gmail.com>
2026-06-08 22:45:10 -0300

    Remove blob and local file news storage implementations
    Deleted all code and configuration for Azure Blob and local file
    news storage backends, including related options and services.

--- src/FreedomBlaze/Configuration/BlobStorageOptions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FreedomBlaze.Configuration;
-
-public class BlobStorageOptions
-{
-    public string AccountName { get; set; } = default!;
-    public string? AccessKey { get; set; } // optional, used when not using DefaultAzureCredential
-}
